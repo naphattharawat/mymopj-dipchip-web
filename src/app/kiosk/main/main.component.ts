fab5a30cc59e4270f0ba8d5220e34c3a3f393806
Naphattharawat S <naphattharawat@gmail.com>
2023-05-12 13:13:56 +0700

    update fix bug

--- src/app/kiosk/main/main.component.ts
+++ src/app/kiosk/main/main.component.ts
@@ -47,13 +47,24 @@ export class MainComponent implements OnInit {
   cardCid: any = '';
   cardFullName: any = '';
   cardBirthDate: any = '';
-  // status = 'offline';
-  status = 'online';
+  status = 'offline';
+  // status = 'online';
   qrdata = '';
   showQR = false;
   sessionId: any;
-  step = 3;
+  step = 1;
+  // step = 5;
   isHr = false;
+  isSave = false;
+
+  cardFname: any = '';
+  cardLname: any = '';
+  sessionId2: any;
+  tel: any;
+  email: any;
+  password: any;
+  password2: any;
+
   @ViewChild(CountdownComponent) counter: CountdownComponent;
 
   constructor(
@@ -139,11 +150,11 @@ export class MainComponent implements OnInit {
         const _payload = JSON.parse(payload.toString());
         if (_payload) {
           if (_topic === 'SMARTCARD/CARD-INSERT') {
-            this.status = 'online';
             await this.setDataFromCard(_payload);
           } else {
             if (_topic === 'SMARTCARD/CARD-REMOVE') {
-              this.status = 'offline';
+              this.clearData();
+
 
             }
           }
@@ -219,12 +230,10 @@ export class MainComponent implements OnInit {
   async setDataFromCard(data) {
     try {
 
-      console.log(data);
-      if (data.cid.length === 13) {
-        this.showQR = true;
+      if (data.isHr) {
+        this.step = 2;
       } else {
-        this.showQR = false;
-
+        this.step = 5;
       }
       this.cardCid = data.cid;
       this.cardFullName = `${data.tname}${data.fname} ${data.lname}`;
@@ -232,8 +241,21 @@ export class MainComponent implements OnInit {
 
       const rnd = new Random();
       // const strRnd = rnd.integer(1111111111, 9999999999);
+      const sess = await this.getSession(data.cid);
+      if (sess.ok) {
+        this.sessionId = sess.sessionId;
+      }
       const sessionId = `${data.cid}-${data.fname}-${data.lname}-${this.sessionId}`;
       this.qrdata = `mymoph://qr?clientId=rLiNPkqoljnPWlQtjoVc&sessionId=${sessionId}`;
+      this.status = 'online';
+      
+      if (data.cid.length === 13) {
+        this.status = 'online';
+        this.showQR = true;
+      } else {
+        this.showQR = false;
+        this.alertService.error('บัตรมีปัญหา กรุณาเสียบใหม่อีกครั้ง', null, 1000);
+      }
       // if (this.cardCid) {
 
       //   // await this.getPatient('CID');
@@ -249,13 +271,89 @@ export class MainComponent implements OnInit {
 
   }
 
+  // clearData() {
+  //   this.cardCid = '';
+  //   this.cardFullName = '';
+  //   this.cardBirthDate = '';
+  //   this.qrdata = '';
+  //   this.status = 'offline';
+  // }
+
   clearData() {
     this.cardCid = '';
+    this.cardFname = '';
+    this.cardLname = '';
     this.cardFullName = '';
     this.cardBirthDate = '';
     this.qrdata = '';
     this.status = 'offline';
+    this.email = '';
+    this.password = '';
+    this.password2 = '';
+    this.tel = '';
+    this.isSave = false;
+    this.step = 1;
   }
 
 
+  async onClickRegister() {
+    this.isSave = true;
+    const pwdPolicy = RegExp(/^\w*(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\w*$/);
+    if (!pwdPolicy.test(this.password) || this.password.length < 8) {
+      this.alertService.error('รหัสผ่านต้องประกอบไปด้วย ตัวใหญ่ ตัวเล็ก ตัวเลข และเกิน 8 ตัวอักษร');
+    } else if (this.password !== this.password2) {
+      this.alertService.error('รหัสผ่านไม่ตรงกัน');
+    } else if (!this.tel) {
+      this.alertService.error('กรุณากรอกเบอร์โทรศัพท์');
+    } else if (!this.email) {
+      this.alertService.error('กรุณากรอกอีเมล');
+    } else {
+      try {
+        const rs: any = await this.kioskService.register(this.cardCid, this.cardFname, this.cardLname, this.email, this.tel, this.password);
+        if (rs.ok) {
+          const l: any = await this.kioskService.login(this.cardCid, this.password);
+          if (l.access_token) {
+            const v: any = await this.kioskService.verify(l.access_token, this.sessionId2);
+            if (v.ok) {
+              this.alertService.success();
+              this.clearData();
+            } else {
+              this.alertService.error(v.error);
+              this.isSave = false;
+            }
+          } else {
+            this.alertService.error(l.error);
+            this.isSave = false;
+          }
+          // const v: any = await this.kioskService.verify(this.cardCid, this.cardFname, this.cardLname, this.sessionId);
+          this.alertService.success();
+          this.clearData();
+          this.isSave = false;
+        } else {
+          this.isSave = false;
+          this.alertService.error(rs.error_description);
+        }
+      } catch (error) {
+        console.log(error);
+        this.isSave = false;
+        this.alertService.serverError();
+      }
+    }
+  }
+
+  changePassword() {
+    this.isSave = true;
+    const pwdPolicy = RegExp(/^\w*(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\w*$/);
+    if (!pwdPolicy.test(this.password) || this.password.length < 8) {
+      this.alertService.error('รหัสผ่านต้องประกอบไปด้วย ตัวใหญ่ ตัวเล็ก ตัวเลข และเกิน 8 ตัวอักษร');
+    } else if (this.password !== this.password2) {
+      this.alertService.error('รหัสผ่านไม่ตรงกัน');
+    } else {
+
+    }
+  }
+
+  renewUser(){
+
+  }
 }
